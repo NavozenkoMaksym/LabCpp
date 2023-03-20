73670ff51ba7c1938538ffac8ae8c916a7eb348e
NavozenkoMaksym <124660070+NavozenkoMaksym@users.noreply.github.com>
2023-03-20 17:32:08 +0200

    Add files via upload

---
+++ Lab4.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+class Numbers {
+private:
+    int* x;
+public:
+    Numbers(int& size);
+    ~Numbers();
+    int* get() { return x; }
+};
+
+Numbers::Numbers(int& size) {
+    x = new int[size];
+    cout << "Видiлення пам'ятi " << x << endl;
+    for (int i = 0; i < size; i++) {
+        if (i % 2 == 0) {
+            x[i] = i - 4;
+        }
+        else {
+            x[i] = i - 6;
+        }
+    }
+}
+
+Numbers::~Numbers() {
+    cout << "Звiльнення пам'ятi " << x << endl;
+    delete[] x;
+    x = 0;
+}
+
+int multi(int& size, int* x) {
+    int y = 1;
+    for (int i = 1; i < size; i += 2) {
+        y *= x[i];
+    }
+    return y;
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int size = 8;
+    Numbers a = Numbers(size);
+    cout << multi(size, a.get()) << endl;
+}
